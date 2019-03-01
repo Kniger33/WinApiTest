f7042e9edb13430a53c83d748b86c03864b9514a
Vlad_Shorin <kniger33@gmail.com>
2019-03-01 21:44:33 +0300

    Все, кроме минимакса

--- CW/CW.cpp
+++ CW/CW.cpp
@@ -7,20 +7,31 @@
 #include <string>
 #include <cstdio>
 #include <math.h>
+#include "string.h"
 
+using namespace std;
 
 BOOL RegClass(WNDPROC, LPCTSTR, UINT);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
 HINSTANCE hInstance;
-
 char szMainClass[] = "MainClass";
 char szMainTitle[] = "Cross-Zero";
 
-int WINAPI WinMain(HINSTANCE hInst,
-					HINSTANCE hPrevInstance,
-					LPSTR lpCmdLine,
-					int nCmdShow)
+void DrawLine(HDC, int, int, int, int);
+void DrawGameField(HDC);
+void DrawCross(HDC, int, int);
+void CheckForWinner(HWND);
+void PrintWinner(HWND);
+void ComputerTurn(HDC, int, int);
+
+int xMouse, yMouse;
+
+char gameField[3][3] = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+char symbol;
+char winner[64] = "Not yet";
+
+
+int WINAPI WinMain(HINSTANCE hInst,	HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	MSG msg;
 	HWND hwnd;
@@ -28,13 +39,15 @@ int WINAPI WinMain(HINSTANCE hInst,
 
 	if (!RegClass(WndProc, szMainClass, COLOR_WINDOW)) { return FALSE; }
 
+	const int w = 308, h = 330;
+	int x = (GetSystemMetrics(SM_CXSCREEN) - w) / 2;
+	int y = (GetSystemMetrics(SM_CYSCREEN) - h) / 2;
+
 	hwnd = CreateWindow(szMainClass,
 							szMainTitle,
-							WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-							CW_USEDEFAULT,
-							CW_USEDEFAULT,
-							320,
-							335,
+							WS_VISIBLE | WS_SYSMENU | WS_MINIMIZEBOX,
+							x, y,
+							w, h,
 							0,
 							0,
 							hInstance,
@@ -43,7 +56,8 @@ int WINAPI WinMain(HINSTANCE hInst,
 	if (!hwnd) { return FALSE; }
 
 	while (GetMessage(&msg, 0, 0, 0))
-	{
+	{		
+		CheckForWinner(hwnd);
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
@@ -67,41 +81,53 @@ BOOL RegClass(WNDPROC Proc, LPCTSTR szName, UINT brBackground)
 	return (RegisterClass(&wc) != 0);
 }
 
-BOOL DrawLine(HDC hdc, int x0, int y0, int x, int y) {
-	MoveToEx(hdc, x0, y0, NULL);
-	return LineTo(hdc, x, y);
-}
-
-BOOL DrawCross(HDC hdc, int squareNum) {
-	MoveToEx(hdc, 0, 0, NULL);
-	LineTo(hdc, 100, 100);
-	MoveToEx(hdc, 0, 100, NULL);
-	LineTo(hdc, 100, 0);
-}
-
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
+{	
 	HDC hdc;
-	PAINTSTRUCT ps;
-
-	int xMouse, yMouse;
-	int width, height;
-
-	RECT mainRect;
-
-	GetWindowRect(hwnd, &mainRect);
-	width = mainRect.right - mainRect.left;
-	height = mainRect.bottom - mainRect.top;
+	PAINTSTRUCT ps;		
 
 	switch (msg)
-	{
+	{	
 		case WM_LBUTTONDOWN:
 		{
 			xMouse = LOWORD(lParam);
 			yMouse = HIWORD(lParam);
 
 			hdc = GetDC(hwnd);
-			DrawCross(hdc, 1);
+
+			if (gameField[xMouse / 100][yMouse / 100] != 'O' 
+				&& gameField[xMouse / 100][yMouse / 100] != 'X')
+			{
+				DrawCross(hdc, xMouse / 100, yMouse / 100);
+				symbol = 'X';
+				gameField[xMouse / 100][yMouse / 100] = symbol;
+			}
+			else
+			{
+				char str[64];
+				sprintf(str, "There is already cross/zero \nPlease, try again", xMouse / 100, yMouse / 100);
+				MessageBox(hwnd, (LPCTSTR)str, "XY", MB_OK);
+			}										
+			break;
+		}
+
+		case WM_LBUTTONUP:
+		{
+			/*char str[64];
+			sprintf(str, "0: %c, 1: %c, 2: %c, 3: %c, 4: %c, 5: %c, 6: %c, 7: %c, 8: %c", gameField[0][0], gameField[1][0], gameField[2][0],
+																							gameField[0][1], gameField[1][1], gameField[2][1],
+																							gameField[0][2], gameField[1][2], gameField[2][2]);
+			MessageBox(hwnd, (LPCTSTR)str, "GameField", MB_OK);
+			*/
+			/*sprintf(str, "3: %c, 4: %c, 5: %c", gameField[0][1], gameField[1][1], gameField[2][1]);
+			MessageBox(hwnd, (LPCTSTR)str, "RES", MB_OK);
+			sprintf(str, "6: %c, 7: %c, 8: %c", gameField[0][2], gameField[1][2], gameField[2][2]);
+			MessageBox(hwnd, (LPCTSTR)str, "RES", MB_OK);*/
+			xMouse = LOWORD(lParam);
+			yMouse = HIWORD(lParam);
+
+			hdc = GetDC(hwnd);
+			ComputerTurn(hdc, xMouse, yMouse);
 
 			break;
 		}
@@ -113,24 +139,27 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			
 			hdc = GetDC(hwnd);
 			
-			Ellipse(hdc, 100, 100, 200, 200);
-			
+			if (gameField[xMouse / 100][yMouse / 100] != 'O'
+				&& gameField[xMouse / 100][yMouse / 100] != 'X')
+			{
+				Ellipse(hdc, (xMouse / 100) * 100, (yMouse / 100) * 100, (xMouse / 100 + 1) * 100, (yMouse / 100 + 1) * 100);
+				symbol = 'O';
+				gameField[xMouse / 100][yMouse / 100] = symbol;
+			}
+			else
+			{
+				char str[64];
+				sprintf(str, "There is already cross/zero \nPlease, try again", xMouse / 100, yMouse / 100);
+				MessageBox(hwnd, (LPCTSTR)str, "XY", MB_OK);
+			}			
 			break;
 		}
 
 		case WM_PAINT:
-		{
-			
+		{			
 			hdc = BeginPaint(hwnd, &ps);
-
-			DrawLine(hdc, 100, 0, 100, 300);
-			DrawLine(hdc, 200, 0, 200, 300);
-			DrawLine(hdc, 0, 100, 300, 100);
-			DrawLine(hdc, 0, 200, 300, 200);
-
-
+			DrawGameField(hdc);			//Отрисовка поля
 			EndPaint(hwnd, &ps);
-
 			break;
 		}
 
@@ -142,3 +171,74 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	}
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
+
+//Отрисовка линии
+void DrawLine(HDC hdc, int left, int top, int right, int bottom) 
+{
+	MoveToEx(hdc, left, top, NULL);
+	LineTo(hdc, right, bottom);
+}
+
+//Отрисовка игрового поля
+void DrawGameField(HDC hdc) 
+{
+	DrawLine(hdc, 100, 0, 100, 300);
+	DrawLine(hdc, 200, 0, 200, 300);
+	DrawLine(hdc, 0, 100, 300, 100);
+	DrawLine(hdc, 0, 200, 300, 200);
+}
+
+//Отрисовка креста
+void DrawCross(HDC hdc, int left, int top)
+{
+	DrawLine(hdc, left * 100, top * 100, (left + 1) * 100, (top + 1) * 100);
+	DrawLine(hdc, left * 100, (top + 1) * 100, (left + 1) * 100, top * 100);
+}
+
+//Проверка на победителя
+void CheckForWinner(HWND hwnd)
+{
+	if ((gameField[0][0] == symbol && gameField[1][0] == symbol && gameField[2][0] == symbol) ||
+		(gameField[0][1] == symbol && gameField[1][1] == symbol && gameField[2][1] == symbol) ||
+		(gameField[0][2] == symbol && gameField[1][1] == symbol && gameField[2][2] == symbol) ||
+		(gameField[0][0] == symbol && gameField[0][1] == symbol && gameField[0][2] == symbol) ||
+		(gameField[1][0] == symbol && gameField[1][1] == symbol && gameField[1][2] == symbol) ||
+		(gameField[2][0] == symbol && gameField[2][1] == symbol && gameField[2][2] == symbol) ||
+		(gameField[0][0] == symbol && gameField[1][1] == symbol && gameField[2][2] == symbol) ||
+		(gameField[2][0] == symbol && gameField[1][1] == symbol && gameField[0][2] == symbol)
+		)
+	{
+		if (symbol == 'X') { strcpy(winner, "User"); }
+		else if (symbol == 'O') { strcpy(winner, "Computer"); }
+	}
+	else if ((gameField[0][0] != 'a' && gameField[1][0] != 'a' && gameField[2][0] != 'a') &&
+			 (gameField[0][1] != 'a' && gameField[1][1] != 'a' && gameField[2][1] != 'a') &&
+			 (gameField[0][2] != 'a' && gameField[1][1] != 'a' && gameField[2][2] != 'a'))
+	{
+		strcpy(winner, "Nobody");
+	}	
+	
+	if (strcmp(winner, "User") == 0
+		|| strcmp(winner, "Computer") == 0
+		|| strcmp(winner, "Nobody") == 0
+		)
+	{
+		char str[64];
+		sprintf(str, "Winner: %s", winner);
+		MessageBox(hwnd, (LPCTSTR)str, "Winner", MB_OK);
+		SendMessage(hwnd, WM_CLOSE, 0, 0);
+	}
+}
+
+//Ход компьютера
+void ComputerTurn(HDC hdc, int _xMouse, int _yMouse)
+{
+
+}
+
+BOOL FreeSquare()
+{
+	
+}
+
+
